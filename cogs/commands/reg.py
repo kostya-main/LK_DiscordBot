54488ba8497dc406bd736b4a19bf8bb6bb9c59a3
kostya-main <simkin-kostya@mail.ru>
2024-01-17 22:04:27 +0300

    fix

--- cogs/commands/reg.py
+++ cogs/commands/reg.py
@@ -21,7 +21,8 @@ class Reg(commands.Cog):
     class Registar(discord.ui.Modal, title="Регистрация нового аккаунта"):
         login = discord.ui.TextInput(label="Игровой логин",placeholder="Логин", style=discord.TextStyle.short, required=True, min_length=5, max_length=20)
         password = discord.ui.TextInput(label="Уникальный пароль",placeholder="Пароль", style=discord.TextStyle.short, required=True, min_length=5, max_length=20)
-        birthday = discord.ui.TextInput(label="Дата твоего рождения",placeholder="2015-12-31", style=discord.TextStyle.short, required=True, min_length=8, max_length=10)
+        if config.bot.event_birthday==True:
+            birthday = discord.ui.TextInput(label="Дата твоего рождения",placeholder="2015-12-31", style=discord.TextStyle.short, required=True, min_length=8, max_length=10)
         promoCode = discord.ui.TextInput(label="Код из проморолика",placeholder="Промокод", style=discord.TextStyle.short, required=False, max_length=20)
 
         async def on_submit(self, interaction: discord.Interaction):
@@ -31,12 +32,15 @@ class Reg(commands.Cog):
                     if r[0] and r[1] is None:
                         login = self.login.value
                         password = self.password.value
-                        try:
-                            birthday = parse(self.birthday.value)
-                        except Exception:
-                            await interaction.response.send_message('Вы указали дату не в том формате.')
-                            return
                         promoCode = self.promoCode.value
+                        if config.bot.event_birthday==True:
+                            try:
+                                birthday = parse(self.birthday.value)
+                            except Exception:
+                                await interaction.response.send_message('Вы указали дату не в том формате.')
+                                return
+                        else:
+                            birthday = None
                         r_reg = db.register(interaction.user.id, login, password, birthday)
                         r_promo = db.check_promo(promoCode)
                         if r_promo[1]['enabled'] == 1:
@@ -51,7 +55,7 @@ class Reg(commands.Cog):
                             db.add_money(interaction.user.id, 20)
                             db.add_use_promo(promoCode)
                         if r_reg[0]:
-                            embedVar = discord.Embed(title="Вы успешно зарегистрированы!", description="Вам предоставлен доступ на пробный период в 7 дней. Этого времени должно хватить, чтобы вы смогли составить мнение о проекте. По истечении срока мы предложим вам оформить подписку за символическую плату 100 рублей/месяц. Доступ по подписке помогает нам обеспечить адекватную аудиторию на сервере и больше времени уделять его развитию. Надеемся на ваше понимание и желаем вам приятной игры!", color=config.embedColor)
+                            embedVar = discord.Embed(title="Вы успешно зарегистрированы!", description="Вам предоставлен доступ на пробный период в 7 дней. Этого времени должно хватить, чтобы вы смогли составить мнение о проекте. По истечении срока мы предложим вам оформить подписку за символическую плату 100 рублей/месяц. Доступ по подписке помогает нам обеспечить адекватную аудиторию на сервере и больше времени уделять его развитию. Надеемся на ваше понимание и желаем вам приятной игры!", color=config.bot.embedColor)
                             embedVar.add_field(name="Ссылки на игровой клиент", value="Он необходим для входа на сервер", inline=False)
                             embedVar.add_field(name="Windows", value=f"[Скачать]({config.web.url_launcher_exe})", inline=True)
                             embedVar.add_field(name="Linux/MacOS", value=f"[Скачать]({config.web.url_launcher_jar})", inline=True)
